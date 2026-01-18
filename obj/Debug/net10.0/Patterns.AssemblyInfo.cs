d277cf304bdc70396cc9bca6245cffac12cbf8e0
Mike McDonald <michaelmcd33@gmail.com>
2026-01-18 13:03:42 -0500

    Add gitignore

--- obj/Debug/net10.0/Patterns.AssemblyInfo.cs
+++ obj/Debug/net10.0/Patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7565d2899c5959ebd42b02d0651beba74dc9a335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
