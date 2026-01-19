8643e52934141ab76627b7ba491690d65ffc51fd
Mike McDonald <michaelmcd33@gmail.com>
2026-01-19 14:27:47 -0500

    Modify Functions class

--- Program.cs
+++ Program.cs
@@ -6,11 +6,27 @@ public class Program
 {
     public static void Main()
     {
+        Console.WriteLine("Creating Builder...");
         Functions.CreateBuilder();
+        Console.WriteLine();
+        
+        Console.WriteLine("Creating Chain of Responsibility...");
         Functions.CreateChainOfResponsibility();
+        Console.WriteLine();
+        
+        Console.WriteLine("Creating Factory...");
         Functions.CreateFactory();
+        Console.WriteLine();
+        
+        Console.WriteLine("Creating Observer...");
         Functions.CreateObserver();
+        Console.WriteLine();
+        
+        Console.WriteLine("Creating Singleton...");
         Functions.CreateSingleton();
+        Console.WriteLine();
+        
+        Console.WriteLine("Creating Strategy...");
         Functions.CreateStrategy();
     }
 }
